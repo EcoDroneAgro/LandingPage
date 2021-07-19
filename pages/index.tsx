47445f681e030928cfacc615673efe953e5b7c18
Vinicius Passos <viniciuspa87@hotmail.com>
2021-07-18 23:58:28 -0300

    🔥 Go!

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,17 +13,32 @@ export default function Home() {
       </Head>
 
       <div className={styles.page}>
-        <h1>Eco Drone</h1>
+        <header className={styles.header}>
+          <h1>EcoDrone</h1>
+          <Image src="/assets/logo.jpg" alt="EcoDrone" width='437' height='424' />
+        </header>
+        <div className={styles.squareContainer}>
+          <div className={styles.square}>
+            <Parallax className="custom-class" x={[-100, 0]} tagOuter="figure">
+              <Image src="/assets/trator.png" alt='drone' width='320' height='100'  />
+            </Parallax>
+          </div>
+          <div className={styles.square}>
+            <h1>
+              Teste
+            </h1>
+          </div>
+        </div>
+
+      </div>
+
+      <div className={styles.page}>
+        teste
         <div className={styles.plants}>
           <Parallax className="custom-class" x={[-100, 100]} y={[-30, 30]} tagOuter="figure">
             <Image src="/assets/drone.png" alt='drone' width='92' height='63'  />
           </Parallax>
         </div>
-        <h1>Eco Drone</h1>
-      </div>
-
-      <div className={styles.page}>
-        teste
       </div>
     </div>
   )
